0afc9223a3237fcda3ed76e022a88aed79aec9ba
BROSNA <31793556+BROSNA@users.noreply.github.com>
2017-10-16 16:21:09 +0700

    update file

--- application/views/rentals/v_rentals_search_top_destinations.php
+++ application/views/rentals/v_rentals_search_top_destinations.php
@@ -1,10 +1,15 @@
 <div class="gap gap-small"></div>
             <h3 class="mb20">Vacation Rentals in Popuplar Destinations</h3>
             <div class="row row-wrap">
+
+                <?php 
+                    for ($i=01; $i <=12 ; $i++) { 
+                    ?>
+                   
                 <div class="col-md-3">
                     <div class="thumb">
                         <a class="hover-img" href="#">
-                            <img src="<?php echo base_url();?>public/img/800x600.png" alt="Image Alternative text" title="Gaviota en el Top" />
+                            <img src="<?php echo base_url();?>public/img/gaviota_en_el_top_800x600.jpg" alt="Image Alternative text" title="Gaviota en el Top" />
                             <div class="hover-inner hover-inner-block hover-inner-bottom hover-inner-bg-black hover-hold">
                                 <div class="text-small">
                                     <h5>New York City Hotels</h5>
@@ -15,158 +20,10 @@
                         </a>
                     </div>
                 </div>
-                <div class="col-md-3">
-                    <div class="thumb">
-                        <a class="hover-img" href="#">
-                            <img src="<?php echo base_url();?>public/img/800x600.png" alt="Image Alternative text" title="Sydney Harbour" />
-                            <div class="hover-inner hover-inner-block hover-inner-bottom hover-inner-bg-black hover-hold">
-                                <div class="text-small">
-                                    <h5>Sydney Hotels</h5>
-                                    <p>70284 reviews</p>
-                                    <p class="mb0">263 offers from $83</p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="thumb">
-                        <a class="hover-img" href="#">
-                            <img src="<?php echo base_url();?>public/img/800x600.png" alt="Image Alternative text" title="Street" />
-                            <div class="hover-inner hover-inner-block hover-inner-bottom hover-inner-bg-black hover-hold">
-                                <div class="text-small">
-                                    <h5>Disney World Hotels</h5>
-                                    <p>60999 reviews</p>
-                                    <p class="mb0">471 offers from $71</p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="thumb">
-                        <a class="hover-img" href="#">
-                            <img src="<?php echo base_url();?>public/img/400x300.png" alt="Image Alternative text" title="the journey home" />
-                            <div class="hover-inner hover-inner-block hover-inner-bottom hover-inner-bg-black hover-hold">
-                                <div class="text-small">
-                                    <h5>Seattle Hotels</h5>
-                                    <p>79499 reviews</p>
-                                    <p class="mb0">979 offers from $62</p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="thumb">
-                        <a class="hover-img" href="#">
-                            <img src="<?php echo base_url();?>public/img/800x600.png" alt="Image Alternative text" title="lack of blue depresses me" />
-                            <div class="hover-inner hover-inner-block hover-inner-bottom hover-inner-bg-black hover-hold">
-                                <div class="text-small">
-                                    <h5>Miami Hotels</h5>
-                                    <p>78475 reviews</p>
-                                    <p class="mb0">263 offers from $73</p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="thumb">
-                        <a class="hover-img" href="#">
-                            <img src="<?php echo base_url();?>public/img/800x600.png" alt="Image Alternative text" title="waipio valley" />
-                            <div class="hover-inner hover-inner-block hover-inner-bottom hover-inner-bg-black hover-hold">
-                                <div class="text-small">
-                                    <h5>Sydney Hotels</h5>
-                                    <p>59206 reviews</p>
-                                    <p class="mb0">266 offers from $71</p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="thumb">
-                        <a class="hover-img" href="#">
-                            <img src="<?php echo base_url();?>public/img/800x600.png" alt="Image Alternative text" title="the best mode of transport here in maldives" />
-                            <div class="hover-inner hover-inner-block hover-inner-bottom hover-inner-bg-black hover-hold">
-                                <div class="text-small">
-                                    <h5>Virginia Beach Hotels</h5>
-                                    <p>57718 reviews</p>
-                                    <p class="mb0">618 offers from $70</p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="thumb">
-                        <a class="hover-img" href="#">
-                            <img src="<?php echo base_url();?>public/img/800x600.png" alt="Image Alternative text" title="Upper Lake in New York Central Park" />
-                            <div class="hover-inner hover-inner-block hover-inner-bottom hover-inner-bg-black hover-hold">
-                                <div class="text-small">
-                                    <h5>Atlantic City Hotels</h5>
-                                    <p>68699 reviews</p>
-                                    <p class="mb0">651 offers from $52</p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="thumb">
-                        <a class="hover-img" href="#">
-                            <img src="<?php echo base_url();?>public/img/800x600.png" alt="Image Alternative text" title="Viva Las Vegas" />
-                            <div class="hover-inner hover-inner-block hover-inner-bottom hover-inner-bg-black hover-hold">
-                                <div class="text-small">
-                                    <h5>Las Vegas</h5>
-                                    <p>72189 reviews</p>
-                                    <p class="mb0">399 offers from $52</p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="thumb">
-                        <a class="hover-img" href="#">
-                            <img src="<?php echo base_url();?>public/img/800x600.png" alt="Image Alternative text" title="El inevitable paso del tiempo" />
-                            <div class="hover-inner hover-inner-block hover-inner-bottom hover-inner-bg-black hover-hold">
-                                <div class="text-small">
-                                    <h5>Budapest</h5>
-                                    <p>64272 reviews</p>
-                                    <p class="mb0">709 offers from $66</p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="thumb">
-                        <a class="hover-img" href="#">
-                            <img src="<?php echo base_url();?>public/img/800x600.png" alt="Image Alternative text" title="new york at an angle" />
-                            <div class="hover-inner hover-inner-block hover-inner-bottom hover-inner-bg-black hover-hold">
-                                <div class="text-small">
-                                    <h5>Boston</h5>
-                                    <p>65639 reviews</p>
-                                    <p class="mb0">480 offers from $69</p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-3">
-                    <div class="thumb">
-                        <a class="hover-img" href="#">
-                            <img src="<?php echo base_url();?>public/img/800x600.png" alt="Image Alternative text" title="196_365" />
-                            <div class="hover-inner hover-inner-block hover-inner-bottom hover-inner-bg-black hover-hold">
-                                <div class="text-small">
-                                    <h5>Paris</h5>
-                                    <p>70002 reviews</p>
-                                    <p class="mb0">566 offers from $78</p>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
+                
+                <?php
+                        }
+                    ?>
+                
+               
             </div>
